a2e2e23f816c094fb0bfafb00db9cc13b4e72038
Mazin Moosa <109205331+mason-san@users.noreply.github.com>
2025-02-18 09:16:38 +0530

    Update assignment_code_02.c
    Now, even though assignment 2 is not refactored, It has been working now.

--- assignment_code_02.c
+++ assignment_code_02.c
@@ -1,26 +1,17 @@
 #include <stdio.h>  
 
-void readDetails(int *number_of_products, int weights[]){ 
-    printf("Enter number of products: "); 
-    scanf("%d", number_of_products); 
-    printf("Enter weights (in grams): ");   
-    printf("THIs is number of producgts: %d", (*number_of_products)); 
-    for (int i = 0 ; i < (*number_of_products); i++){
-        scanf("%d", weights[i]); 
-    } 
-} 
-
 int main(){ 
     //init 
     int number_of_products = 0; 
     int weights[25]; 
   
     //read details  
-    readDetails(&number_of_products,weights);  
+    printf("Enter number of products: "); 
+    scanf("%d", &number_of_products); 
+    printf("Enter weights (in grams): ");   
     for (int i = 0 ; i < number_of_products; i++){
-        printf("%d\n", weights[i]); 
-    }
-
+        scanf("%d", &weights[i]); 
+    } 
     //Output example  
     int accepted_number = 0 ; 
     int rejected_number = 0 ; 
