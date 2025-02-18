7b9c07d081b9c65d906b50c45fefd06024eb0ca9
mason-san <mazinmoosa63@gmail.com>
2025-02-18 08:50:32 +0530

    assignment-03

---
+++ assignment_code_03.c
@@ -0,0 +1,31 @@
+#include <stdio.h>  
+#include <string.h> 
+
+int main(){ 
+    //init 
+    int number_of_vehicles; 
+    char vehicles[500];     
+
+    //Reading values 
+    printf("Enter number of vehicles: "); 
+    scanf("%d", &number_of_vehicles);  
+    printf("Enter vehicle types: ");  
+    scanf("%s", vehicles); 
+
+    //calculation  
+    int total_toll = 0; 
+    for (int i= 0; i < number_of_vehicles; i++){ 
+        if (strcmp(vehicles[i], "Car")){
+            total_toll+=50; 
+        } else if (strcmp(vehicles[i], "Bike")){
+            total_toll+=20; 
+        }else{
+            total_toll+=100; 
+        }
+    }
+
+    //display
+    printf("Total Toll Collection: %d\n", total_toll); 
+
+    return 0; 
+}
