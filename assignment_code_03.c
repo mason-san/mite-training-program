da8a75e8a56d694370fc70bc9bee8e2595c5def3
mason-san <mazinmoosa63@gmail.com>
2025-02-18 10:53:12 +0530

    Assignment 3 working version

--- assignment_code_03.c
+++ assignment_code_03.c
@@ -3,30 +3,29 @@
 
 int main(){ 
     //init 
-    int number_of_vehicles; 
-    char vehicles[500];     
+    int number_of_vehicles;   
     //Reading values 
     printf("Enter number of vehicles: "); 
     scanf("%d", &number_of_vehicles);  
     printf("Enter vehicle types: ");  
-    scanf("%s", vehicles); 
+    
     //calculation  
     int total_toll = 0; 
-    for (int i= 0; i < number_of_vehicles; i++){ 
-        if (strcmp(vehicles[i], "Car")){
+     
+    for (int i= 0; i < number_of_vehicles; i++){  
+        char vehicle[100];
+        scanf("%s", &vehicle); 
+
+        if (strcmp(vehicle, "Car") == 0){
             total_toll+=50; 
-        } else if (strcmp(vehicles[i], "Bike")){
-            total_toll+=20; 
-        }else{
-            total_toll+=100; 
+        } else if (strcmp(vehicle, "Truck") == 0){
+            total_toll+= 100; 
+        } else {
+            total_toll+= 20; 
         }
+
     }
 
-    //display
-    printf("Total Toll Collection: %d\n", total_toll); 
-
-    return 0; 
-}
     //display
     printf("Total Toll Collection: %d\n", total_toll); 
 
