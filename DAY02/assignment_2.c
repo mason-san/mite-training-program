08376938b8ee0ae0e6bc7c61aa705ea520f5915a
mason-san <mazinmoosa63@gmail.com>
2025-02-19 11:27:14 +0530

    proper assignment 2 dynamic memory allocation

--- DAY02/assignment_2.c
+++ DAY02/assignment_2.c
@@ -7,42 +7,40 @@ struct student{
 };   
 typedef struct student *p_student;  
 
-void studentDetails(int number_of_students, p_student pointer_student, struct student total_students[5]){
-
+void studentDetails(int number_of_students, p_student pointer_student){
     for(int i = 0 ; i < number_of_students; i++){ 
-        pointer_student = (p_student)malloc(sizeof(struct student));
-        pointer_student= &total_students[i]; 
-
         printf("Enter student %d Name: ", i+1); 
         scanf("%s", pointer_student->name); 
         printf("Enter Marks: "); 
         scanf("%d", &pointer_student->marks); 
+        pointer_student++; 
     } 
 } 
 
-void printDetails(int number_of_students, p_student pointer_student, struct student total_students[5]){
-
+void printDetails(int number_of_students, p_student pointer_student){
     for(int i= 0; i < number_of_students; i++){
-        pointer_student = &total_students[i]; 
         printf("%s - %d\n", pointer_student->name, pointer_student->marks); 
+        pointer_student++; 
     }
 
 }
 
 int main(){ 
     //init
-    struct student total_students[5];  
     p_student pointer_student; 
-
     int number_of_students; 
 
     //entering values 
     printf("Enter number of students: ");  
-    scanf("%d", &number_of_students); 
-    studentDetails(number_of_students, pointer_student, total_students); 
+    scanf("%d", &number_of_students);
+    pointer_student =  (p_student) malloc(sizeof(struct student) * number_of_students); 
+    studentDetails(number_of_students, pointer_student); 
      
     //output  
-    printDetails(number_of_students, pointer_student, total_students); 
+    printDetails(number_of_students, pointer_student); 
+   
+    //freeing the memory
+    free(pointer_student); 
 
     return 0; 
 }
