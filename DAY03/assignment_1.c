c6a744610f4a145a89b1bfdcbfc8719d74011c74
mason-san <mazinmoosa63@gmail.com>
2025-02-20 09:40:45 +0530

    Refactored assignment 1

--- DAY03/assignment_1.c
+++ DAY03/assignment_1.c
@@ -1,6 +1,5 @@
 #include <stdio.h>  
 #include <string.h> 
-#define MAX 50
 
 struct passenger{
     char name[30]; 
@@ -8,24 +7,16 @@ struct passenger{
     char dest[30]; 
 };
 
-void enterPassengerDetails(int number_of_passengers, struct passenger passengers[MAX]){
+typedef struct passenger Passenger; 
+
+void enterPassengerDetails(int number_of_passengers, Passenger passengers[]){
     for(int i = 0 ; i < number_of_passengers; i++){
         printf("Passenger %d: ", i+1); 
         scanf("%s %d %s", passengers[i].name, &passengers[i].age, passengers[i].dest);
     }
 }
 
-int main(){
-    //init
-    int number_of_passengers;
-    struct passenger passengers[MAX];
-
-    //input
-    printf("Enter the number of passengers: ");
-    scanf("%d", &number_of_passengers);  
-    enterPassengerDetails(number_of_passengers, passengers);
-
-    //sorting 
+void displaySortedPassengerList(int number_of_passengers, Passenger passengers[]){
     printf("Sorted List (by destination):\n"); 
     for(int i= 0; i < number_of_passengers; i++){
         struct passenger temp; 
@@ -38,13 +29,9 @@ int main(){
             }
         }
     } 
+}
 
-    //print Sorted list 
-    for(int i = 0 ; i < number_of_passengers; i++){
-        printf("%s - %s\n", passengers[i].name, passengers[i].dest); 
-    } 
-
-    //searching destination  
+void getDestinationDetails(int number_of_passengers, Passenger passengers[]){
     char search[30]; 
     printf("Enter destination to search: "); 
     scanf("%s", search);
@@ -53,5 +40,28 @@ int main(){
             printf("Passenger travelling to %s: %s\n", search, passengers[i].name); 
         }
     }
+}
+
+int main(){
+    //init
+    int number_of_passengers;
+    Passenger passengers[1000];
+
+    //input
+    printf("Enter the number of passengers: ");
+    scanf("%d", &number_of_passengers);  
+    enterPassengerDetails(number_of_passengers, passengers);
+
+    //sorting 
+    displaySortedPassengerList(number_of_passengers, passengers); 
+
+    //print Sorted list 
+    for(int i = 0 ; i < number_of_passengers; i++){
+        printf("%s - %s\n", passengers[i].name, passengers[i].dest); 
+    } 
+
+    //searching destination   
+    getDestinationDetails(number_of_passengers, passengers);
+
     return 0; 
 }
