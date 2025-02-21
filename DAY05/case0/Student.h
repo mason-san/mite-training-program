40fcff0281babde29d8509ce07ddab22556fdae6
mason-san <mazinmoosa63@gmail.com>
2025-02-21 11:26:38 +0530

    making main files

---
+++ DAY05/case0/Student.h
@@ -0,0 +1,9 @@
+struct Student_t{
+    int rollNo; 
+    char name[50]; 
+    int maths, physics, chemistry, cs; 
+    int totalMarks; 
+    float percentage; 
+    char grade; 
+}; 
+typedef struct Student_t Student;  
